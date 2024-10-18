feb790062de8782776cc8c6ed0486c95b6b3c660
jlopezlash <jlopezlash1@educacion.navarra.es>
2024-10-18 08:29:54 +0200

    Update Ejercicio05.py

---
+++ Ejercicio05.py
@@ -0,0 +1,3 @@
+#Escribir un programa que pida al usuario que introduzca una frase en la consola y muestre por pantalla la frase invertida.
+Frase = input('Escribeme una frase ')
+print(Frase[::-1])
